3552650bb552baeb1103ffd8e2adc44b51e75419
Natasha <nlackas12@gmail.com>
2023-03-10 18:41:21 -0600

    updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -14,16 +14,11 @@
 
 
 
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
+
+
 
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -123,6 +118,10 @@ ${data.usage}
 ## Contribute
 
 ${data.contribute}
+
+## License
+
+${data.license}
 
 ## Tests
 
---
+++ Develop/sample_README.md
@@ -0,0 +1,46 @@
+
+# jdfksjf
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contibute](#contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
+
+## Description
+
+ sdjflksj
+
+
+## Installation
+
+undefined
+
+## Usage
+
+sjdkljl
+
+## Contribute
+
+jksldj
+
+## License
+
+MIT
+
+## Tests
+
+kjdksf
+
+## Questions
+
+If you have questions you can reach me at jdkfl.
+
+
+You can also see more of my projects at https://github.com/jljsd.
+
+
